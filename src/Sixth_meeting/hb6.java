8b66b9148e58d7a35987a20cc41b82c01d8d0b2c
heebum99 <heebum9955@naver.com>
2022-11-22 18:21:43 +0900

    자바 스터디(희범) - 단어 Quiz 게임

--- src/Sixth_meeting/hb6.java
+++ src/Sixth_meeting/hb6.java
@@ -22,10 +22,10 @@ class Word {
 }
 
 public class hb6 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Vector<Word> v = new Vector<>();
-        int[] question = new int[4];
+    static Scanner scanner = new Scanner(System.in);
+    static Vector<Word> v = new Vector<>();
+    static int[] question = new int[4];
+    public static void Add_Word(){
         v.add(new Word("love", "사랑"));
         v.add(new Word("animal", "동물"));
         v.add(new Word("bear", "곰"));
@@ -40,39 +40,48 @@ public class hb6 {
         v.add(new Word("error", "오류"));
         v.add(new Word("example", "보기"));
         v.add(new Word("deal", "거래"));
+    }
+    public static void  Make_Question(){
+        for (int i = 0; i < 4; i++) {
+            int random_index = (int) (Math.random() * (v.size()));
+            question[i] = random_index;
+            for (int j = 0; j < i; j++) { //보기 중복 방지
+                if (question[i] == question[j]) {
+                    i--;
+                    continue;
+                }
+            }
+        }
+    }
+    public static void Game(){
+        int answer_index = question[(int) (Math.random() * question.length)]; //0~3 인덱스 중 정답 인덱스 설정
+        System.out.println(v.get(answer_index).getEng() + "?");
+        for (int i = 0; i < question.length; i++) {
+            if (i == question.length - 1) {
+                System.out.print("(" + (i + 1) + ")" + v.get(question[i]).getKor() + " >> ");
+            } else {
+                System.out.println("(" + (i + 1) + ")" + v.get(question[i]).getKor());
+            }
+        }
+        int select = scanner.nextInt();
+        if (select == -1) {
+            System.out.println("\"명품영어\"를 종료합니다.");
+            System.exit(0);
+        } else {
+            if (v.get(question[select - 1]).getKor() == v.get(answer_index).getKor()) {
+                System.out.println("Excellent!!");
+            } else {
+                System.out.println("No!!");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Add_Word(); //단어 추가
         System.out.println("\"명품영어\"의 단어 테스트를 시작합니다. -1을 입력하면 종료합니다.");
         System.out.println("현재 " + v.size() + "개의 단어가 들어있습니다.");
         while (true) {
-            for (int i = 0; i < 4; i++) {
-                int random_index = (int) (Math.random() * (v.size()));
-                question[i] = random_index;
-                for (int j = 0; j < i; j++) { //보기 중복 방지
-                    if (question[i] == question[j]) {
-                        i--;
-                        continue;
-                    }
-                }
-            }
-            int answer_index = question[(int) (Math.random() * question.length)]; //0~3 인덱스 중 정답 인덱스 설정
-            System.out.println(v.get(answer_index).getEng() + "?");
-            for (int i = 0; i < question.length; i++) {
-                if (i == question.length - 1) {
-                    System.out.print("(" + (i + 1) + ")" + v.get(question[i]).getKor() + " >> ");
-                } else {
-                    System.out.println("(" + (i + 1) + ")" + v.get(question[i]).getKor());
-                }
-            }
-            int select = scanner.nextInt();
-            if (select == -1) {
-                System.out.println("\"명품영어\"를 종료합니다.");
-                System.exit(0);
-            } else {
-                if (v.get(question[select - 1]).getKor() == v.get(answer_index).getKor()) {
-                    System.out.println("Excellent!!");
-                } else {
-                    System.out.println("No!!");
-                }
-            }
+            Make_Question();
+            Game();
         }
     }
 }
